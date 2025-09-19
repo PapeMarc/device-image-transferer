930c268551e3bf58292098010d211d66e122631b
Marc Pape <marc.pape-info@web.de>
2025-09-19 16:42:13 +0200

    Implemented QR-Code-generation on click by Button on the Reciever Page. Next TODO: Implement Network-Listener.

--- device-image-transferer/ViewModels/RecieverPageViewModel.cs
+++ device-image-transferer/ViewModels/RecieverPageViewModel.cs
@@ -1,7 +1,48 @@
 ﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using QRCoder;
+using System.Net;
+using System.Net.Sockets;
+
 namespace device_image_transferer.ViewModels
 {
-    public partial class RecieverPageViewModel
+    public partial class RecieverPageViewModel: ObservableObject
     {
+        [ObservableProperty]
+        ImageSource qrImage;
+
+        [RelayCommand]
+        public void GenerateQRCode()
+        {
+            string networkInfo = GetNetworkInformation();
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            using (QRCodeData qrCodeData = qrGenerator.CreateQrCode(networkInfo, QRCodeGenerator.ECCLevel.Q))
+            using (PngByteQRCode qrCode = new PngByteQRCode(qrCodeData))
+            {
+                byte[] qrCodeBytes = qrCode.GetGraphic(
+                            pixelsPerModule: 20,
+                            drawQuietZones: false
+                        );
+                QrImage = ImageSource.FromStream(() => new MemoryStream(qrCodeBytes));
+            }
+            OnPropertyChanged(nameof(QrImage));
+        }
+
+        private string GetNetworkInformation()
+        {
+            IPHostEntry hostEntry = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress address in hostEntry.AddressList)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return address.ToString();
+                }
+            }
+            return "Error";
+
+        }
+
     }
 }
